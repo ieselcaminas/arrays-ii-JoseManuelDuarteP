bf11925380db0571899fb853716180ae2f32f093
josduaper <josduaper@alu.edu.gva.es>
2024-10-29 12:24:53 +0100

    a

--- src/main/Comunes.java
+++ src/main/Comunes.java
@@ -1,19 +1,22 @@
 public class Comunes {
-    public static void main(String[] args) {
-        int[] num1 = {5,5,1,8,3};
-        int[] num2 = {5,8,3};
+
+    public  static String comunes(int[] num1, int[] num2) {
         String reultado = "";
         for (int i = 0; i < num1.length; i++) {
             for (int j = 0; j < num2.length; j++) {
 
 
                 if (num1[i] == num2[j]) {
-                    reultado += num1[i];
+                    reultado += num1[i] + " ";
                     break;
                 }
             }
 
         }
-        System.out.println(reultado);
+        return reultado;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
