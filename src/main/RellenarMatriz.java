46173123257ab724e49fd10fcd781bee3d302fab
josduaper <josduaper@alu.edu.gva.es>
2024-11-08 12:11:36 +0100

    SegundoMayor

---
+++ src/main/RellenarMatriz.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class RellenarMatriz {
+
+   // public static int[] inverso(int[] lista) {
+
+   // }
+
+    public static void main(String[] args) {
+        int[][] nums = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {}
+        }
+    }
+}
